9b4d0184c05441dd89d1c79876110a8e082b74fc
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-06-04 12:57:48 +0600

    Navbar design done

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,79 @@
+import { FaUserCircle } from "react-icons/fa";
+import { IoCart } from "react-icons/io5";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar mt-5 rounded-lg text-white bg-gradient-to-r from-[#591BFF] from-100% to-[#7B4AFF] to-67%">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Events</a>
+              </li>
+              <li>
+                <a>About us</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <p className="text-xl w-fit pr-[92px] py-2 md:border-r-4 border-dashed relative">
+            Event<span className="font-bold">Host</span>
+            <div className="hidden md:block w-5 h-5 rounded-full bg-white absolute left-44 -bottom-6"></div>
+            <div className="hidden md:block w-5 h-5 rounded-full bg-white absolute left-44 -top-6"></div>
+          </p>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Events</a>
+            </li>
+            <li>
+              <a>About us</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end relative">
+          <div className="py-2 pl-[84px] md:border-l-4 border-dashed flex gap-2">
+            <FaUserCircle className="text-3xl"></FaUserCircle>
+            <IoCart className="text-3xl"></IoCart>
+          </div>
+          <div className="hidden md:block w-5 h-5 rounded-full bg-white absolute right-36 -top-6 "></div>
+          <div className="hidden md:block w-5 h-5 rounded-full bg-white absolute right-36 -bottom-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
 
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="w-11/12 mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
