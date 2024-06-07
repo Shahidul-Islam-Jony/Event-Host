2ab0af0f5a03fd59593944f4c825e6d09c8fd7c6
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-06-07 09:57:31 +0600

    Event Shedule responsive done

--- src/components/Home/EventShedule/EventShedule.jsx
+++ src/components/Home/EventShedule/EventShedule.jsx
@@ -4,8 +4,8 @@ import clock from "../../../assets/image/clock.jpg"
 
 const EventShedule = () => {
   return (
-    <div className="bg-blue-600 flex gap-8 rounded-2xl">
-      <div className="h-80">
+    <div className="bg-blue-600 flex flex-col md:flex-row gap-8 rounded-2xl">
+      <div className="lg:h-80">
         <img src={eventHost} className="w-full md:h-[100%] rounded-xl" alt="" />
       </div>
       <div className="text-white relative z-50 overflow-hidden p-7">
@@ -16,7 +16,7 @@ const EventShedule = () => {
         <p className="flex items-center gap-2 mb-2"><FaCircleCheck /> Host Your Event With Easy Steps!</p>
         <p className="flex items-center gap-2 mb-2"><FaCircleCheck /> Host Your Event With Easy Steps!</p>
         <div>
-            <img src={clock} className="w-44 h-44 rounded-full absolute -bottom-10 -right-10" alt="" />
+            <img src={clock} className="w-20 h-20 lg:w-44 lg:h-44 rounded-full absolute -bottom-7 md:-bottom-6 md:-right-6 lg:-bottom-10 lg:-right-10" alt="" />
         </div>
       </div>
     </div>
